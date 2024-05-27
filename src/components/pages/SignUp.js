30539c03c33b8e6a9f6c805e8fa6a3da0ab64636
parth24917 <krishparth26@gmail.com>
2024-05-27 05:50:45 +0530

    initial

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import ContactForm from './form.js';
+
+export default function SignUp() {
+  return (
+    <>
+    
+  <ContactForm/>
+
+    </>
+  
+
+);
+}
---
+++ src/components/pages/form.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import '../../App.css';
+import './form.css';
+import axios from 'axios';
+
+export default function ContactForm() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const handleSubmit = () =>{
+    const data = {
+      name: name,
+      email:email,
+      password: password
+    }
+    axios.post("http://localhost:4000/users/register", data).then(() =>{
+      alert("Form submitted hooray!!");
+    })
+  }
+  return (
+    <form className='contact-form'>
+      <h2 className='contact-header'>Contact Us</h2>
+      <label htmlFor='name'>Name:</label>
+      <input type='text' id='name' value={name} onChange={e => setName(e.target.value)} />
+
+      <label htmlFor='email'>Email:</label>
+      <input type='email' id='email'  value={email} onChange={e => setEmail(e.target.value)} />
+
+      <label htmlFor='password'>Password:</label>
+      <textarea id='password'  value={password} onChange={e => setPassword(e.target.value)} />  
+
+      <button type='submit' onClick={handleSubmit}>Submit</button>
+
+    </form>
+  );
+}
