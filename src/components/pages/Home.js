d69319fb84486ad4b8ba15b237bbd47683b29f83
parth24917 <72308605+parth24917@users.noreply.github.com>
2024-07-25 17:43:28 +0530

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,10 +3,12 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
+import { button } from './Button';
 
 function Home() {
   return (
     <>
+      <button>Sample</button>
       <HeroSection />
       <Cards />
       <Footer />
