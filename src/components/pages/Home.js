1fcb7781994eeaafc8c4627f54b05f59386cbdfb
parth24917 <72308605+parth24917@users.noreply.github.com>
2024-08-31 15:26:28 +0530

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
-import { Button } from './Button';
+
 
 function Home() {
   return (
