e539b160773f3d77e65ddd07dc71111b718bd2aa
Parth Kumar <parth.kumar.coox@gmail.com>
2025-02-27 19:19:43 +0530

    Updated state after form submission

--- src/components/pages/form.js
+++ src/components/pages/form.js
@@ -6,18 +6,21 @@ import axios from 'axios';
 export default function ContactForm() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
-  const [message, setMessage] = useState("")  // Changed password to message
+  const [message, setMessage] = useState("")  
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = {
       name: name,
       email: email,
-      message: message  // Updated to send message instead of password
+      message: message  
     };
   
     axios.post("http://localhost:4000/api/users/register", data)
       .then(() => {
         alert("We will contact you shortly.");
+        setName("");
+        setEmail("");
+        setMessage("");
       })
       .catch((error) => {
         if (error.response && error.response.status === 400) {
@@ -37,8 +40,8 @@ export default function ContactForm() {
       <label htmlFor='email'>Email:</label>
       <input type='email' id='email'  value={email} onChange={e => setEmail(e.target.value)} />
 
-      <label htmlFor='message'>Message: </label>  {/* Updated label */}
-      <textarea id='message' value={message} onChange={e => setMessage(e.target.value)} />  {/* Updated input */}
+      <label htmlFor='message'>Message: </label>  
+      <textarea id='message' value={message} onChange={e => setMessage(e.target.value)} /> 
 
       <button type='submit' onClick={(e) => handleSubmit(e)}>Submit</button>
     </form>
